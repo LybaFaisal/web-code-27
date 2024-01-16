4c188bed4f885cb51793330ab0e2d69a147530ac
Lyba <faisallyba06@gmail.com>
2024-01-16 13:48:20 +0500

    Logic

--- file12.js
+++ file12.js
@@ -22,3 +22,9 @@ for(let i=2 ; i<10 ; i++)
         document.write(i + "<br>")
     }
 }
+//firstly check the outer loop i=2, i<10 then enters the inner loop , then check j<i (2<2)
+//not true, lets check the next condition, i===j (2===2), yes true (prints 2).
+//then again check the 1st loop there is an i++ (now i == 3). i<10(3<10) yes true,
+//then j<i (2<3) yes true check the if condition( !break) , loop#2 continiues, j++ (j==3)
+// j<i (3<3) not true, check the outer if(i===j) (3==3) yes true..
+//then comes the outer loop there is an i++, (now i==4)!
